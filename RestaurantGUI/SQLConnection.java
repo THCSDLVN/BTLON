5ec1ddae827d1c904fed263415390f1bf582718c
khung9895 <khung9895@gmail.com>
2015-10-28 01:43:05 +0700

    update

--- RestaurantGUI/SQLConnection.java
+++ RestaurantGUI/SQLConnection.java
@@ -1,4 +1,4 @@
-package javalearn;
+package gui;
 import java.sql.DriverManager;
 import java.sql.Connection;
 
--- RestaurantGUI/SqlArrayList.java
+++ RestaurantGUI/SqlArrayList.java
@@ -1,4 +1,4 @@
-package javalearn;
+package gui;
 
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
