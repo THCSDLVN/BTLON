c3b0d4f43991cfc4ade9b9473468e75de947ccda
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2015-11-06 21:52:23 +0700

    Database moi

---
+++ Data/InsertRestaurant.sql
@@ -0,0 +1,18 @@
+INSERT INTO Restaurant
+values('RES0001','123'),
+('RES0002','ABC'),
+('RES0003','LOTTE'),
+('RES0004','KFC'),
+('RES0005','Nhà hàng Thiên Lộc'),
+('RES0006','Nhà hàng MasterChef'),
+('RES0007',' Nhà Hàng The Sun'),
+('RES0008','Nhà hàng VINGON 22'),
+('RES0009','Nhà hàng Sing'),
+('RES0010','Eureka Coffee & Fastfood Shop'),
+('RES0011','Nhà hàng Vạn Tuế Thăng Long'),
+('RES0012','V2 Club ( Bar - Restaurant - Cafe )'),
+('RES0013','Sumo BBQ'),
+('RES0014','Nhà hàng Hải Long Vương'),
+('RES0015','Nhà hàng Hoàng Đế'),
+('RES0016','Nhà hàng Bắc Bộ'),
+('RES0017','McDonnald');
