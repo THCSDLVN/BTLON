b85a800dcfc08902232c0e1878e498f359998c1a
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2015-11-14 11:22:56 +0700

    update server frame

--- Server/ServerFrame/serverframe/ServerFrame.java
+++ Server/ServerFrame/serverframe/ServerFrame.java
@@ -4,37 +4,32 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JFrame;
 import javax.swing.SwingConstants;
-import javax.swing.JOptionPane;
-import javax.swing.BorderFactory;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.WindowConstants;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.font.TextAttribute;
 import java.awt.Cursor;
 import java.awt.Dimension;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseAdapter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import java.util.Map;
-
 import dataqueryframe.DataQueryFrame;
 
 import errdiagram.ERRDiagram;
+import buttonflag.ButtonFlag;
 
 public class ServerFrame extends JFrame implements ServerFrameInterface{
+	public ButtonFlag buttonFlag = new ButtonFlag();
 	public ServerFrame(){
 		super("ServerFrame");
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setResizable(false);
 		setVisible(true);
-		setPreferredSize(new Dimension(550,390));
+		setPreferredSize(new Dimension(580,386));
 		pack();
 
 		getContentPane().setLayout(null);
@@ -42,14 +37,19 @@ public class ServerFrame extends JFrame implements ServerFrameInterface{
 		getContentPane().add(buttonExit);
 		getContentPane().add(buttonDiagram);
 		getContentPane().add(buttonQuery);
+		getContentPane().add(backGroundLabel);
 
 		appNameLabel.setIcon(new ImageIcon("/home/mylaptop/AppDatabase/DatabaseOfResApp/Resource/AppName.png"));
-		appNameLabel.setBounds(130, 10, 300, 68);
+		appNameLabel.setBounds(20, 0, 300, 68);
 
-		buttonExit.setBackground(Color.WHITE);
-		buttonExit.setBounds(200, 250, 160, 70);
-		buttonExit.setFont(new Font("Ubuntu", 1, 18));
-		buttonExit.setForeground(new Color(22, 112, 245));
+		backGroundLabel.setIcon(new ImageIcon("/home/mylaptop/AppDatabase/DatabaseOfResApp/Resource/background.png"));
+		backGroundLabel.setBounds(-1, 0, 580, 390);
+
+		buttonExit.setBackground(new Color(254, 236, 181));
+		buttonExit.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		buttonExit.setBounds(450, 160, 110, 30);
+		buttonExit.setFont(new Font("Ubuntu", 1, 15));
+		buttonExit.setForeground(new Color(237, 21, 15));
 		buttonExit.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent ae){
 				Runtime run = Runtime.getRuntime();
@@ -63,20 +63,27 @@ public class ServerFrame extends JFrame implements ServerFrameInterface{
 			}
 		});
 
-		buttonQuery.setBackground(Color.WHITE);
-		buttonQuery.setBounds(100, 140, 160, 70);
-		buttonQuery.setFont(new Font("Ubuntu", 1, 18));
-		buttonQuery.setForeground(new Color(22, 112, 245));
-		buttonQuery.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent ae){
-				new DataQueryFrame();			
-			}
-		});
+		buttonQuery.setBackground(new Color(254, 236, 181));
+		buttonQuery.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		buttonQuery.setBounds(450, 60, 110, 30);
+		buttonQuery.setFont(new Font("Ubuntu", 1, 14));
+		buttonQuery.setForeground(new Color(237, 21, 15));
+		if(buttonQuery.getActionListeners().length < 1){
+			buttonQuery.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent ae){
+					if(buttonFlag.buttonQueryFlag == 1){
+						buttonFlag.buttonQueryFlag = 0;
+						new DataQueryFrame(buttonFlag);	
+					}		
+				}
+			});
+		}
 
-		buttonDiagram.setBackground(Color.WHITE);
-		buttonDiagram.setBounds(300, 140, 160, 70);
-		buttonDiagram.setFont(new Font("Ubuntu", 1, 18));
-		buttonDiagram.setForeground(new Color(22, 112, 245));
+		buttonDiagram.setBackground(new Color(254, 236, 181));
+		buttonDiagram.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		buttonDiagram.setBounds(450, 110, 110, 30);
+		buttonDiagram.setFont(new Font("Ubuntu", 1, 15));
+		buttonDiagram.setForeground(new Color(237, 21, 15));
 		buttonDiagram.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent ae){
 				new ERRDiagram();			
--- Server/ServerFrame/serverframe/ServerFrameInterface.java
+++ Server/ServerFrame/serverframe/ServerFrameInterface.java
@@ -6,6 +6,7 @@ import javax.swing.JLabel;
 public interface ServerFrameInterface {
 	public JButton buttonExit = new JButton("Exit");
 	public JButton buttonQuery = new JButton("Data Query");	
-	public JButton buttonDiagram = new JButton("ERRDiagram");
+	public JButton buttonDiagram = new JButton("Diagram");
 	public JLabel appNameLabel = new JLabel();
+	public JLabel backGroundLabel = new JLabel();
 }
