1128867e17401864f7d5b23776555b6756310003
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2015-11-14 11:17:39 +0700

    update button flag

--- Server/ServerFrame/buttonflag/ButtonFlag.java
+++ Server/ServerFrame/buttonflag/ButtonFlag.java
@@ -2,7 +2,4 @@ package buttonflag;
 
 public class ButtonFlag{
 	public int buttonQueryFlag = 1;
-	public int buttonInsertFlag = 1;
-	public int buttonDeleteFlag = 1;
-	public int buttonUpdateFlag = 1;
 }
