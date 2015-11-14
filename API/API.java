8d9b6655aa75259804b716f51a2810e80ab7df15
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2015-11-14 11:34:49 +0700

    update API

--- API/API.java
+++ API/API.java
@@ -23,29 +23,24 @@ condition = tương tự như khi viết cấu lệnh SQL (VD: AID = 'ACC0001' a
 tableName, condition bắt buộc phải khác rỗng
 */
 
-public int updateDataQuery(String tableName, String set, String value, String condition)
+public int updateDataQuery(String tableName, String setColumn, String setNewValue, String condition)
 /*
 hàm sử dụng để update dữ liệu - Sử dụng câu lệnh UPDATE 
 tableName = Bảng1 (Chỉ update dữ liệu của 1 bảng duy nhất)
-set = tên của cột muốn update dữ liệu(chi update dc 1 cột duy nhất)
-value = giá trị mới của cột
+setColumn = tên của cột muốn update dữ liệu(chi update dc 1 cột duy nhất)
+setNewValue = giá trị mới của cột
 condition = tương tự như câu lệnh SQl (AID = 'ACC0001')
 
 tableName và set phải khác rỗng
 condition có thể bằng rỗng("") - nhưng buộc phải có trong câu lệnh
 */
 
-public int insertDataQuery(String tableName, String value1, String value2, String value3, String value4, String value5, String value6, String value7, String value8)
+public int insertDataQuery(String tableName, String value1, String value2, String value3, String value4, String value5, String value6)
 /*
 hàm sử dụng để nhập dữ liệu vào bảng - Sử dụng câu lệnh INSERT 
 tableName = Bảng1 (Chỉ insert được dữ liệu của 1 bảng duy nhất)
 Các thành phần khóa chính ở các bảng phải khác rỗng
-value1 ---> value8
-Restaurant  -- ("Restaurant",rid,restaurantName,address,phone,face,rate,resName,resPass)
+value1 ---> value6
 Account     -- ("Account",aid,fullName,userName,password,phone,face,"","")
-FoodShip    -- ("FoodShip",rid,aid,foodlist,address,time,"","","")
-Menu        -- ("Menu",fid,foodName,"","","","","","")
-Provide     -- ("Provide",rid,fid,cost,"","","","","")
-Reservation -- ("Reservations",rid,aid,foodlist,time,"","","","")
 tên các bảng, cột chỉ chứa : Chữ cái, chữ số, _
 */
