c6f42b2cf21e4796129153b38e8cd674a9bb954b
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2015-12-03 21:55:27 +0700

    Create Installer

---
+++ Installer/InstallClient/installerClient.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo tar xvfz ResAppClient.tar.gz -C /usr/share/
+sudo tar xvfz DesktopClient.tar.gz -C /usr/share/applications
+echo "Program installed.!"
+read
+exit
---
+++ Installer/InstallSever/installerServer.sh~
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo tar xvfz ResAppServer.tar.gz -C /usr/share/
+sudo tar xvfz DesktopServer.tar.gz -C /usr/share/applications
+echo "Program installed.!"
+read
+exit
